2156b715fc48b632bf31f073be461e8279f6d50d
SeddikBellamine <bellamine.seddik@gmail.com>
2023-05-05 15:00:15 +0200

    add start script

---
+++ start_example.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the GitHub repo
+git clone https://github.com/iExecBlockchainComputing/my-data-protector-app.git
+
+# Change to the project directory
+cd my-data-protector-app
+
+# Install dependencies using npm
+npm install
+
+# Start the development server using npm
+npm start
